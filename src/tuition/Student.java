33400686ac5693705b0f050a5f120eb1a4dff106
wesamsaleh <wesamssaleh@gmail.com>
2021-10-03 19:32:35 -0400

    Framework for student class

--- src/tuition/Student.java
+++ src/tuition/Student.java
@@ -1,4 +1,26 @@
 package tuition;
 
+/**
+ * Student class defines the basic data for a student.
+ * Parent class of Resident and NonResident students.
+ * @author Wesam Saleh
+ */
+
 public class Student {
+    Profile profile;
+
+    /**
+     * Constructor for student class
+     * @param profile - the profile of the student including their name and major
+     */
+    public Student(Profile profile){
+        this.profile = profile;
+    }
+
+    /**
+     * Do nothing method for calculating the tuition due
+     */
+    public void tuitionDue() {
+
+    }
 }
