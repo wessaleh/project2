ffbca67c8423414373cca1d8f633ee1bbdd619a6
wesamsaleh <wesamssaleh@gmail.com>
2021-10-03 19:46:41 -0400

    Framework for Profile and Roster classes

--- src/tuition/Profile.java
+++ src/tuition/Profile.java
@@ -1,4 +1,21 @@
 package tuition;
 
+/**
+ * The profile class defines the data for a student
+ * @author Wesam Saleh
+ */
+
 public class Profile {
+    private String name;
+    private Major major; // 5 majors and 2-characters each: CS, IT, BA, EE, ME
+
+    /**
+     * Constructor for Profile of a student
+     * @param name - the name of the student
+     * @param major - the major of the student
+     */
+    public Profile(String name, Major major){
+        this.name = name;
+        this.major = major;
+    }
 }
--- src/tuition/Roster.java
+++ src/tuition/Roster.java
@@ -1,4 +1,48 @@
 package tuition;
 
 public class Roster {
+    private Student[] roster;
+    private int size; //keep track of the number of students in the roster
+
+    /**
+     * Constructor for a student roster. Initial capacity is 4.
+     */
+    public Roster(){
+        roster = new Student[4];
+        size = 0;
+    }
+
+    /**
+     * Finds a student in the roster
+     * @param student - the student to be found
+     * @return - the index of the student in the roster if found
+     */
+    private int find(Student student) {
+        return 0;
+    }
+
+    /**
+     * Increases the student roster by 4
+     */
+    private void grow() {
+
+    }
+
+    /**
+     * Adds a student to the roster
+     * @param student - the student to add
+     * @return true if the student was added, false if not
+     */
+    public boolean add(Student student) {
+        return true;
+    }
+
+    /**
+     * Removes a student from the roster
+     * @param student - the student to remove
+     * @return true if the student was removed, false if not
+     */
+    public boolean remove(Student student) {
+        return true;
+    }
 }
