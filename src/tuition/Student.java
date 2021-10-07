efca19a8a9b07b787247b5f63765fe7152c162ce
wesamsaleh <wesamssaleh@gmail.com>
2021-10-07 18:50:54 -0500

    provided framework for student class

--- src/tuition/Student.java
+++ src/tuition/Student.java
@@ -9,6 +9,9 @@ package tuition;
 public class Student {
     private Profile profile;
     private int creditHours;
+    private double tuition;
+    private double payments;
+    private Date lastPaymentDate;
 
     /**
      * Constructor for student class
@@ -17,6 +20,27 @@ public class Student {
     public Student(Profile profile, int creditHours){
         this.profile = profile;
         this.creditHours = creditHours;
+        this.lastPaymentDate = null;
+        this.tuition = 0;
+        this.payments = 0;
+    }
+
+    /**
+     * Sets the tuition of this Student
+     * @param tuitionAmount - the amount to set the tuition
+     */
+    public void setTuition(double tuitionAmount) {
+        this.tuition = tuitionAmount;
+    }
+
+    /**
+     * Makes a payment for this student on the given date
+     * @param payment - payment to be made
+     * @param paymentDate - date payment was made
+     */
+    public void makePayment(double payment, Date paymentDate){
+        this.payments += payment;
+        this.lastPaymentDate = paymentDate;
     }
 
     /**
