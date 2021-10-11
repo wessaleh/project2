6c9924c1d439ed057c26bfbef2889ddb27b672f0
wesamsaleh <wesamssaleh@gmail.com>
2021-10-11 17:09:15 -0400

    Updated Date test

--- src/unitTesting/DateTest.java
+++ src/unitTesting/DateTest.java
@@ -8,17 +8,15 @@ class DateTest {
 
     @org.junit.jupiter.api.Test
     void isValid() {
-        Date wrongDay = new Date("12/00/2000");
-        Date wrongDay2 = new Date("12/32/2000");
-        Date wrongMonth = new Date("00/27/2000");
-        Date wrongMonth2 = new Date("13/27/2000");
+        Date wrongDay = new Date("12/00/2021");
+        Date wrongDay2 = new Date("12/32/2021");
+        Date wrongMonth = new Date("00/27/2021");
+        Date wrongMonth2 = new Date("13/27/2021");
         Date futureDate = new Date("12/27/2021");
-        Date notLeapYear = new Date("2/29/2001");
+        Date notLeapYear = new Date("2/29/2021");
 
-        Date leapYear = new Date("2/29/2004");
-        Date correctDate = new Date("12/27/2000");
+        Date correctDate = new Date("09/27/2021");
         Date todaysDate = new Date();
-        Date todaysDate2 = new Date();
 
         assertFalse(wrongDay.isValid());
         assertFalse(wrongDay2.isValid());
@@ -27,10 +25,7 @@ class DateTest {
         assertFalse(futureDate.isValid());
         assertFalse(notLeapYear.isValid());
 
-
-        assertTrue(leapYear.isValid());
         assertTrue(correctDate.isValid());
         assertTrue(todaysDate.isValid());
-        assertTrue(todaysDate2.isValid());
     }
 }
